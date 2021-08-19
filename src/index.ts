b6ece54b3f5116039227532a56625a2a9eff2833
Alexander <alex_osilva@hotmail.com>
2021-08-19 04:10:05 -0300

    feat: add basic express settings

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const api = express()
+api.get('/test', (req, res) => {
+    res.json({ success: true })
+})
+api.listen(5000, () => console.log('Express listening'))
